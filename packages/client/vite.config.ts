8ae7cbb1d608cb7fbce3fbdf4192ab35adcec39c
Durmus A <durmusaliyuksek@gmail.com>
2026-02-07 19:39:21 +0100

    Connected frontend and backend

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+// import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   server: {
+      proxy: {
+         '/api': 'http://localhost:3000',
+
+         // /api/hello -> http://localhost:3000/api/hello
+      },
+   },
+});
